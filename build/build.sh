ceeae9ccaa56a6b68d98b33b256c995b47225461
EC2 Default User <ec2-user@ip-172-31-18-14.us-west-2.compute.internal>
2016-09-25 01:14:58 +0000

    Add gitignore and cleaner uploads to amazon

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-mkdir -p ../bin
-cd ../bin
+mkdir -p ../binTmp
+cd ../binTmp
 cmake ../src && make
 
+mkdir -p ../bin
+
+cp ../binTmp/Main ../bin/
 cp ../thirdparty/dependencies/sio_libs/lib*.so* ../bin/
 cp ../thirdparty/dependencies/boost_libs/lib*.so* ../bin/
 cp ../thirdparty/gamelift/lib/amazon_linux_x64/libaws-cpp-sdk-gamelift-server.so ../bin/
